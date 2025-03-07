e83cc70cfe6b80e267800513e37e48c22bae2a43
ClevertonJaber <solo-sage@hotmail.com>
2025-03-07 10:05:54 -0300

    ProjetoLogin

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+    rememberMe: new FormControl(false)
+  });
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Form submitted', this.loginForm.value);
+      // Aqui você adicionaria a lógica de autenticação
+    } else {
+      this.loginForm.markAllAsTouched();
+    }
+  }
+
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+}
